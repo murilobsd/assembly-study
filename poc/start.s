6521843dde8eca817075507124ccf9b1ecfe9a73
murilo ijanc <mbsd@m0x.ru>
2024-06-09 23:08:25 -0300

    chore: retab

--- poc/start.s
+++ poc/start.s
@@ -2,7 +2,7 @@
 
 .text
 start:
-	mov $777, %eax # movemos o numero 777 para o registrador eax
-			 # eax e conhecido como registrador acumulador
-			 # usado em operacoes 32bits
-	ret
+        mov $777, %eax   # movemos o numero 777 para o registrador eax
+                         # eax e conhecido como registrador acumulador
+                         # usado em operacoes 32bits
+        ret
